10894081b750c3394791b2f85dffcfebec806a45
NistorAlexandru <rotsinurdnaxela@gmail.com>
2024-10-27 21:03:50 +0200

    tema 3

--- tema 3.py
+++ tema 3.py
@@ -5,17 +5,38 @@ comenzi = ["guias", "ceafa", "ceafa", "papanasi", "ceafa"]  # coada FIFO
 tavi = ["tava"] * 7  # stiva LIFO
 istoric_comenzi = []
 
-for student in studenti[::1]:
-    istoric_comenzi.append(studenti.pop(0))
+istoric_comenzi_de_printat = []
 
-print (dict (zip (istoric_comenzi, comenzi)))
-print (studenti)
+n = len(studenti)
+
+for n in range(n):
+    istoric_comenzi_de_printat.append([studenti[n] + " a comandat " + comenzi[n] ])
+
+for n in range(n+1):
+    tavi.pop()
+    istoric_comenzi.append(comenzi.pop(0))
+    studenti.pop(0)
+
+print(istoric_comenzi_de_printat)
+print ("La coada avem " + str(len(studenti))+ " studenti")
+
+print("- au mai ramas " + str(len(tavi)) + " tavi")
+print ('- au ramas ' + str(meniu.count('ceafa') - istoric_comenzi.count('ceafa')) + ' portii de ceafa')
+print ('- au ramas ' + str(meniu.count('papanasi') - istoric_comenzi.count('papanasi')) + ' portii de papanasi')
+print ('- au ramas ' + str(meniu.count('guias') - istoric_comenzi.count('guias')) + ' portii de guias')
+
+update_meniu =[meniu.count('papanasi') - istoric_comenzi.count('papanasi'), meniu.count('ceafa') - istoric_comenzi.count('ceafa'),meniu.count('guias') - istoric_comenzi.count('guias')]
+
+for n in range (3):
+    print(' Mai sunt '+ preturi[n][0] + ': ' + str(bool(update_meniu[n]>0)))
+
+incasari = preturi[0][1] * istoric_comenzi.count('papanasi') + preturi[1][1] * istoric_comenzi.count('ceafa') + preturi[2][1] * istoric_comenzi.count('guias')
+
+print("S-au incasat in total: "+ str(incasari) + ' lei')
 
 
-# COMENZI
-
-# Afisați numele studentului și ce a comandat.
-# Eliminați studentul din coadă după ce comanda lui a fost procesată.
-# Eliminați tava din stivă odată ce comanda a fost servită.
-# Eliminați comanda din coadă.
-# Actualizați istoricul comenzilor prin adăugarea fiecărei comenzi în lista istoric_comenzi.
+preturi_filtrate =[]
+for element in preturi:
+     if element[1] < 8:
+        preturi_filtrate.append(element)
+print('Preturi de cel mult 7 lei: ',  preturi_filtrate)
